d2a664afa749e567167dba26ea8e484869a460d7
Aayushi Chaudhry <aayushi.chaudhry@berkeley.edu>
2022-10-24 22:43:03 -0700

    half completed

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,23 +1,28 @@
-import { useState } from 'react';
-import React from 'react';
-import Menu from './Menu';
-import Block from './Block';
+import { useState } from "react";
+import React from "react";
+import Menu from "./Menu";
+import Block from "./Block";
 
 const Feed = () => {
-    /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
-    // Refer to Hint 2 for more help!
+  /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
+  // Refer to Hint 2 for more help!
 
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+  /* Use the map() function to render multiple Blocks! */
+  const posts = [null]; // TODO: edit this variable
+  const [numPosts, addPost] = useState(posts);
 
-    return (
-        <div>
-            <Menu></Menu>
+  const addBlock = (el) => {
+    <Block color="el"></Block>;
+  };
 
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
-        </div>
-    );
-}
+  return (
+    <div>
+      <Menu></Menu>
+
+      {/* Below is where all of your Blocks should render! */}
+      {posts.map(addBlock)}
+    </div>
+  );
+};
 
 export default Feed;
